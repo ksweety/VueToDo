ec8bdbece2df8df43c83453c98991dd0b2fda56e
ksweety <kahlilw03@gmail.com>
2023-05-19 14:52:15 -0400

    IT IS OVERRR

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,10 @@ import {ref, watch} from "vue"
   }
 
  let newtodo= ref("")
+
+ function clearTodos(){
+  todos.value = []
+ }
  
  
  function popUpTodo () {
@@ -35,43 +39,53 @@ import {ref, watch} from "vue"
 
  } 
  function deleteTodo(index) { 
-  todos.value.splice(index,1)
+  todos.value.splice(index,1) 
 
  }
+
+ function activeFilter (todo) {
+    return todo.complete == false
+  }
 </script>
 
 <template> 
-  <input name="filter" type="radio" value="all" v-model="filter"> 
-  <label>All</label>
-  <input name="filter" type="radio" value="active" v-model="filter">
- <label>Active</label> 
- <input name="filter"  type="radio" value="completed" v-model="filter">
- <label>Completed</label>
-  <h1>My Todo List</h1>  
+  <p v-if="todos.length> 0">
+  </p>
+  <p>{{ todos.filter(activeFilter).length }} items left</p>
+  <label class="form-control"><input name="filter" type="radio" value="all" v-model="filter">All</label>
+  <label class="form-control"><input name="filter" type="radio" value="active" v-model="filter">Active</label> 
+  <label class="form-control"><input name="filter"  type="radio" value="completed" v-model="filter">Completed</label>
+
+  <h1>My Todo List</h1>    
+ 
+
   <ul class="dripster">
   <li v-for="(todo, index) in todos.filter(todoFilter)">
-    <label class="container"><input type="checkbox" v-model="todo.complete"><span class="checkmark"></span></label>
-    
-    <button @click="deleteTodo(index)">🗑</button> 
+    <label class="container"><input type="checkbox" class="boxdrip" v-model="todo.complete"><span class="checkmark"></span></label>
+    <button @click="deleteTodo(index)" class="dripbutton">❌</button> 
+   
+
 
     <span :class="{completed: todo.complete}">{{ todo.text }}</span>
 
-  <button @click="deleteTodo()">🗑</button>
+ 
   </li> 
   </ul> 
+  
   <input v-model="newtodo" class="button " @keydown.enter="popUpTodo">
-  <button @click="popUpTodo" class="button">Add Todo</button>
+  <p><button @click="popUpTodo" class="button">Add Todo</button> 
+    <button @click="clearTodos" class="button">Clear All</button>
+  </p> 
+
+   
 </template>
 
 <style> 
-@import url('https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap');
-
-body{
+@import url('https://fonts.googleapis.com/css2?family=Bungee&display=swap');
+  body {
   background-color: bisque; 
-  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; 
-
-
-}
+  font-family: "Bungee", cursive;
+  }
 /* Customize the label (the container) */
 .container {
   display: block;
@@ -144,20 +158,128 @@ body{
   color:#ccc
 } 
 .dripster {
-  background-color: beige; 
-  border-radius: 200px;
+  background-color: #12b8ce; 
+  border-radius: 100px;
 } 
 
 .button {
-  background-color: #b1731c; /* Green */
+  background-color:#12b8ce75; /* Green */
   border: none;
-  color: white;
-  padding: 15px 32px;
+  color: black;
+  padding: 15px 50px;
   text-align: center;
   text-decoration: none;
   display: inline-block;
   font-size: 16px; 
-  border-radius: 20px;
+  border-radius: 12px; 
+  font-family: "Bungee", cursive;
+  float: center;
+} 
+h1 {
+  text-align: center; 
+  text-decoration:underline
+}
+body {
+  background-color: #ffffff;
+background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 800 800'%3E%3Cdefs%3E%3CradialGradient id='a' cx='400' cy='400' r='50%25' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ffffff'/%3E%3Cstop offset='1' stop-color='%230EF'/%3E%3C/radialGradient%3E%3CradialGradient id='b' cx='400' cy='400' r='70%25' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ffffff'/%3E%3Cstop offset='1' stop-color='%230FF'/%3E%3C/radialGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='800' height='800'/%3E%3Cg fill-opacity='.8'%3E%3Cpath fill='url(%23b)' d='M998.7 439.2c1.7-26.5 1.7-52.7 0.1-78.5L401 399.9c0 0 0-0.1 0-0.1l587.6-116.9c-5.1-25.9-11.9-51.2-20.3-75.8L400.9 399.7c0 0 0-0.1 0-0.1l537.3-265c-11.6-23.5-24.8-46.2-39.3-67.9L400.8 399.5c0 0 0-0.1-0.1-0.1l450.4-395c-17.3-19.7-35.8-38.2-55.5-55.5l-395 450.4c0 0-0.1 0-0.1-0.1L733.4-99c-21.7-14.5-44.4-27.6-68-39.3l-265 537.4c0 0-0.1 0-0.1 0l192.6-567.4c-24.6-8.3-49.9-15.1-75.8-20.2L400.2 399c0 0-0.1 0-0.1 0l39.2-597.7c-26.5-1.7-52.7-1.7-78.5-0.1L399.9 399c0 0-0.1 0-0.1 0L282.9-188.6c-25.9 5.1-51.2 11.9-75.8 20.3l192.6 567.4c0 0-0.1 0-0.1 0l-265-537.3c-23.5 11.6-46.2 24.8-67.9 39.3l332.8 498.1c0 0-0.1 0-0.1 0.1L4.4-51.1C-15.3-33.9-33.8-15.3-51.1 4.4l450.4 395c0 0 0 0.1-0.1 0.1L-99 66.6c-14.5 21.7-27.6 44.4-39.3 68l537.4 265c0 0 0 0.1 0 0.1l-567.4-192.6c-8.3 24.6-15.1 49.9-20.2 75.8L399 399.8c0 0 0 0.1 0 0.1l-597.7-39.2c-1.7 26.5-1.7 52.7-0.1 78.5L399 400.1c0 0 0 0.1 0 0.1l-587.6 116.9c5.1 25.9 11.9 51.2 20.3 75.8l567.4-192.6c0 0 0 0.1 0 0.1l-537.3 265c11.6 23.5 24.8 46.2 39.3 67.9l498.1-332.8c0 0 0 0.1 0.1 0.1l-450.4 395c17.3 19.7 35.8 38.2 55.5 55.5l395-450.4c0 0 0.1 0 0.1 0.1L66.6 899c21.7 14.5 44.4 27.6 68 39.3l265-537.4c0 0 0.1 0 0.1 0L207.1 968.3c24.6 8.3 49.9 15.1 75.8 20.2L399.8 401c0 0 0.1 0 0.1 0l-39.2 597.7c26.5 1.7 52.7 1.7 78.5 0.1L400.1 401c0 0 0.1 0 0.1 0l116.9 587.6c25.9-5.1 51.2-11.9 75.8-20.3L400.3 400.9c0 0 0.1 0 0.1 0l265 537.3c23.5-11.6 46.2-24.8 67.9-39.3L400.5 400.8c0 0 0.1 0 0.1-0.1l395 450.4c19.7-17.3 38.2-35.8 55.5-55.5l-450.4-395c0 0 0-0.1 0.1-0.1L899 733.4c14.5-21.7 27.6-44.4 39.3-68l-537.4-265c0 0 0-0.1 0-0.1l567.4 192.6c8.3-24.6 15.1-49.9 20.2-75.8L401 400.2c0 0 0-0.1 0-0.1L998.7 439.2z'/%3E%3C/g%3E%3C/svg%3E");
+background-attachment: fixed;
+background-size: cover;
+}
+li {
+  text-align: center;
+}
+.dripbutton {
+  background-color: transparent;
+  border-color: transparent; 
+  padding: 20px;
+}  
+.boxdrip { 
+  background-color: #578bb6;  
+
+
 }
 
+#drippy  {
+  text-align: right;
+}
+:root {
+  --form-control-color: rgb(51, 146, 153);
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+form {
+  display: grid;
+  place-content: center;
+  min-height: 100vh;
+}
+
+.form-control {
+  font-family: "Bungee", cursive;
+  font-size: 2rem;
+
+  line-height: 1.1;
+  display: grid;
+  grid-template-columns: 1em auto;
+  gap: 0.5em;
+}
+
+.form-control + .form-control {
+  margin-top: 1em;
+}
+
+.form-control:focus-within {
+  color: var(--form-control-color);
+}
+
+input[type="radio"] {
+  /* Add if not using autoprefixer */
+  -webkit-appearance: none;
+  /* Remove most all native input styles */
+  appearance: none;
+  /* For iOS < 15 */
+  background-color: var(--form-background);
+  /* Not removed via appearance */
+  margin: 0;
+
+  font: inherit;
+  color: currentColor;
+  width: 1.15em;
+  height: 1.15em;
+  border: 0.15em solid currentColor;
+  border-radius: 50%;
+  transform: translateY(-0.075em);
+
+  display: grid;
+  place-content: center;
+}
+
+input[type="radio"]::before {
+  content: "";
+  width: 0.65em;
+  height: 0.65em;
+  border-radius: 50%;
+  transform: scale(0);
+  transition: 120ms transform ease-in-out;
+  box-shadow: inset 1em 1em var(--form-control-color);
+  /* Windows High Contrast Mode */
+  background-color: CanvasText;
+}
+
+input[type="radio"]:checked::before {
+  transform: scale(1);
+}
+
+input[type="radio"]:focus {
+  outline: max(2px, 0.15em) solid currentColor;
+  outline-offset: max(2px, 0.15em);
+}
 </style>
